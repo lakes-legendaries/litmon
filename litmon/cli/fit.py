bea027256cc8018c78e724656cdfb91a07d279dd
Mike Powell <mike@lakeslegendaries.com>
2021-11-20 17:43:32 -0500

    revamping fit (still need to revamp ArticleScorer for this to work tho

--- litmon/cli/fit.py
+++ litmon/cli/fit.py
@@ -1,7 +1,11 @@
 """Fit ML model"""
 
+from pandas import DataFrame, read_csv
+
 from litmon.model import ArticleScorer
 from litmon.utils.cli import cli
+from litmon.utils.cloud import Azure
+from litmon.utils.dates import drange
 
 
 class ModelFitter:
@@ -9,8 +13,13 @@ class ModelFitter:
 
     Parameters
     ----------
-    dbase_fname: str, optional, default='data/dbase_fit.csv'
-        Input filename for training data
+    date_range: str
+        months to use for training. Format: YYYY/mm-YYYY/mm
+    dbase_dir: str, optional, default='data'
+        directory to load database files from.
+        :code:`dbase_fname=f'{dbase_dir}/{year}-{month:02d}{dbase_suffix}.csv'`
+    dbase_suffix: str, optional, default=''
+        suffix for each database file. See :code:`dbase_dir`
     model_fname: str, optional, default='data/model'
         Output filename for saving trained model to. This should NOT have a
         file extension, because a :code:`model_fname.bin` and a
@@ -21,11 +30,25 @@ class ModelFitter:
     def __init__(
         self,
         /,
-        dbase_fname: str = 'data/dbase_fit.csv',
+        date_range: str,
+        *,
+        dbase_dir: str = 'data',
+        dbase_suffix: str = '-fit',
         model_fname: str = 'data/model',
         **kwargs
     ):
-        model = ArticleScorer(**kwargs).fit(dbase_fname)
+
+        # load in data
+        data = DataFrame()
+        for year, month in drange(date_range):
+            fname = f'{dbase_dir}/{year:4d}-{month:02d}{dbase_suffix}.csv'
+            Azure.download(fname)
+            data = data.append(read_csv(fname))
+
+        # fit model
+        model = ArticleScorer(**kwargs).fit(data)
+
+        # save model
         model.save(model_fname)
 
 
