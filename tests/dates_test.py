3221420bef7b3c8c7de81aabcef4f2ada90a08d8
Mike Powell <mike@lakeslegendaries.com>
2021-11-20 17:44:50 -0500

    updating tests

--- tests/dates_test.py
+++ tests/dates_test.py
@@ -1,28 +1,14 @@
-from datetime import date
-
-from pandas import DataFrame
-
-from litmon import get_date, get_dates
+from litmon import drange
 
 
-def test_get_date():
-    assert(get_date('2012-08-03') == date(2012, 8, 3))
-    assert(get_date('2012/08/03') == date(2012, 8, 3))
-
-
-def test_get_dates():
-    df = DataFrame(
-        [
-            ['', '1991/02/12', ''],
-            ['', '1992-03-02', ''],
-        ],
-        columns=['a', 'date', 'b'],
-    )
-    dates = get_dates(df['date'])
-    assert(dates[0] == date(1991, 2, 12))
-    assert(dates[1] == date(1992, 3, 2))
+def test():
+    drng = drange('2013/11-2014/02')
+    assert(len(drng) == 4)
+    assert(drng[0] == (2013, 11))
+    assert(drng[1] == (2013, 12))
+    assert(drng[2] == (2014, 1))
+    assert(drng[3] == (2014, 2))
 
 
 if __name__ == '__main__':
-    test_get_date()
-    test_get_dates()
+    test()
